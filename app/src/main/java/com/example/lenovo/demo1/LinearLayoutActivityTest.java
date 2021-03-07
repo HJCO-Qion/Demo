d3709f3e0a84135ca2e81a0339251511e70ccb11
HJCO-Qion <cedupeice19693@126.com>
2021-03-07 17:40:32 +0800

    LinearLayout and RelativeLayout test.

---
+++ app/src/main/java/com/example/lenovo/demo1/LinearLayoutActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.lenovo.demo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lenovo on 2018/10/9.
+ */
+public class LinearLayoutActivityTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.linear_layout_activity_test);
+    }
+}
--- app/src/main/java/com/example/lenovo/demo1/MainActivity.java
+++ app/src/main/java/com/example/lenovo/demo1/MainActivity.java
@@ -18,11 +18,23 @@ public class MainActivity extends Activity {
 //        this.IntentAndResultActivityTest();//Intent带返回结果测试
 //        this.LifeCycleActivityTest();//生命周期测试
 //        this.StandardActivityTest();//测试活动的启动模式
-        this.SimpleChatActivityTest();//简单的实现手机与Linux服务器的通讯聊天
-
-
+//        this.SimpleChatActivityTest();//简单的实现手机与Linux服务器的通讯聊天
+//        this.LinearLayoutActivityTest();//LinearLayout测试
+        this.RelativeLayoutActivityTest();//RelativeLayout测试
 
 //        this.IntentAndImgResultActivityTest();//使用Intent从图库获取图片并显示，未完成。
+    }
+
+    public void RelativeLayoutActivityTest(){
+        Intent intent = new Intent(MainActivity.this, RelativeLayoutActivityTest.class);
+        this.startActivity(intent);
+        this.finish();
+    }
+
+    public void LinearLayoutActivityTest(){
+        Intent intent = new Intent(MainActivity.this, LinearLayoutActivityTest.class);
+        this.startActivity(intent);
+        this.finish();
     }
 
     public void SimpleChatActivityTest(){
